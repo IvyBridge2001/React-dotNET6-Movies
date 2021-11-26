c28ade01997701737c7d50be2ba65fe9293f59ab
dpessoax <danielx.pessoa@intel.com>
2021-11-26 17:29:27 -0300

    Added API

---
+++ MoviesAPI/MoviesAPI/Controllers/GenresController.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using MoviesAPI.Services;
+
+namespace MoviesAPI.Controllers
+{
+    public class GenresController : Controller
+    {
+        private readonly IRepository repository;
+
+        public GenresController(IRepository repository)
+        {
+            this.repository = repository;
+        }
+
+    }
+}
