5779b0ea4f13c94e51db2494e7ed6ecebcf716bf
Daniel Pessoa <coolivybridge@hotmail.com>
2021-11-27 22:30:00 -0300

    Adding Genre controller methods

--- MoviesAPI/MoviesAPI/Controllers/GenresController.cs
+++ MoviesAPI/MoviesAPI/Controllers/GenresController.cs
@@ -1,9 +1,12 @@
 ﻿using Microsoft.AspNetCore.Mvc;
 using MoviesAPI.Services;
+using MoviesAPI.Entities;
 
 namespace MoviesAPI.Controllers
 {
-    public class GenresController : Controller
+    //[Route("api/[controller]")]
+    [Route("api/genres")]
+    public class GenresController : ControllerBase
     {
         private readonly IRepository repository;
 
@@ -11,6 +14,38 @@ namespace MoviesAPI.Controllers
         {
             this.repository = repository;
         }
+
+        [HttpGet] // /api/genres
+        [HttpGet("list")] // api/genres/list/
+        [HttpGet("/allgenres")] // /allgenres
+        public List<Genre> Get()
+        {
+            return repository.GetAllGenres();
+        }
+
+        //[HttpGet("example")]
+        //[HttpGet("{Id:int}/{param2=felipe}")]
+        [HttpGet("{Id}")]
+        public Genre Get(int Id)
+        {
+            var genre = repository.GetGenreById(Id);
+
+            if (genre == null)
+            {
+                //return NotFound();
+            }
+
+            return genre;
+        }
+
+        [HttpPost]
+        public void Post(){}
+
+        [HttpPut]
+        public void Put() { }
+
+        [HttpDelete]
+        public void Delete() { }
 
     }
 }
