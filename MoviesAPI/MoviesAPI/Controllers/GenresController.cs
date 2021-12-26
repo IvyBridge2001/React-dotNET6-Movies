90e3450aee9268971b4ac798f033a0293de58355
dan <dan@DESKTOP-UR7KSST>
2021-12-26 15:11:16 -0300

    Finished exploring hte main concepts

--- MoviesAPI/MoviesAPI/Controllers/GenresController.cs
+++ MoviesAPI/MoviesAPI/Controllers/GenresController.cs
@@ -2,45 +2,59 @@
 using MoviesAPI.Services;
 using MoviesAPI.Entities;
 using Microsoft.AspNetCore.Mvc.ModelBinding;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Authentication.JwtBearer;
+using MoviesAPI.Filters;
 
 namespace MoviesAPI.Controllers
 {
     //[Route("api/[controller]")]
     [Route("api/genres")]
+    [ApiController]
     public class GenresController : ControllerBase
     {
         private readonly IRepository repository;
+        private readonly ILogger<GenresController> logger;
 
-        public GenresController(IRepository repository)
+        public GenresController(IRepository repository, ILogger<GenresController> logger)
         {
             this.repository = repository;
+            this.logger = logger;
         }
 
         [HttpGet] // /api/genres
         [HttpGet("list")] // api/genres/list/
         [HttpGet("/allgenres")] // /allgenres
+        [ResponseCache(Duration = 60)]
+        [ServiceFilter(typeof(MyActionFilter))]
+        //[Authorize(AuthenticationSchemes = JwtBearerDefaults.AuthenticationScheme)]
         public async Task<ActionResult<List<Genre>>> Get()
         {
+            logger.LogInformation("Getting all the genres");
             return await repository.GetAllGenres();
         }
 
         //[HttpGet("example")]
         //[HttpGet("{Id:int}/{param2=felipe}")]
+        //public ActionResult<Genre> Get(int Id, [BindRequired] string param2)
         [HttpGet("{Id}")]
-        public ActionResult<Genre> Get(int Id, [BindRequired] string param2)
+        public ActionResult<Genre> Get(int Id)
         {
             //BindNever will not bring the value form the request
             //BindRequired maskes param obligatory
             //FromHeader,FromBody,FromQuery,FromRoute,FromService
-            if (!ModelState.IsValid)
-            {
-                return BadRequest(ModelState);
-            }
-
+            //if (!ModelState.IsValid)
+            //{
+            //    return BadRequest(ModelState);
+            //}
+            //not necessary because of the apicontroller decorator
+            logger.LogDebug("get by Id method executing...");
             var genre = repository.GetGenreById(Id);
 
             if (genre == null)
             {
+                logger.LogWarning($"Genre with Id {Id} not found");
+                //throw new ApplicationException();
                 return NotFound();
             }
 
@@ -50,6 +64,8 @@ namespace MoviesAPI.Controllers
         [HttpPost]
         public ActionResult Post([FromBody] Genre genre)
         {
+            repository.AddGenre(genre);
+
             return NoContent();
         }
 
--- MoviesAPI/MoviesAPI/Program.cs
+++ MoviesAPI/MoviesAPI/Program.cs
@@ -1,16 +1,64 @@
+using Microsoft.AspNetCore.Authentication.JwtBearer;
+using MoviesAPI.Filters;
 using MoviesAPI.Services;
 
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 
-builder.Services.AddControllers();
+builder.Services.AddControllers(options =>
+{
+    options.Filters.Add(typeof(MyExceptionFilter));
+});
+builder.Services.AddResponseCaching();
+builder.Services.AddAuthentication(JwtBearerDefaults.AuthenticationScheme).AddJwtBearer();
 builder.Services.AddSingleton<IRepository, InMemoryRepository>();
+builder.Services.AddTransient<MyActionFilter>();
+//AddTransient a new instance for every request
+//AddSingleton 1 instance for all requests
+//AddScoped one for each http request
+
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 
 var app = builder.Build();
+
+//app.Run(async context =>
+//{
+//    await context.Response.WriteAsync("Short circuiting the pipeline");
+//});
+
+//app.Use(async (context, next) =>
+//{
+//    using (var swapStream = new MemoryStream())
+//    {
+//        var originalResponseBody = context.Response.Body;
+//        context.Response.Body = swapStream;
+
+//        await next.Invoke();
+
+//        swapStream.Seek(0, SeekOrigin.Begin);
+//        string responseBody = new StreamReader(swapStream).ReadToEnd();
+//        swapStream.Seek(0, SeekOrigin.Begin);
+
+//        await swapStream.CopyToAsync(originalResponseBody);
+//        context.Response.Body = originalResponseBody;
+
+//        var logger = new ILogger<Program>();
+        
+//        logger.LogInformation(responseBody);  
+//    }
+//});
+
+//app.Map("/map1", (app) =>
+//{
+//    app.Run(async context =>
+//    {
+//        await context.Response.WriteAsync("Short circuiting the pipeline");
+//    });
+//}
+//);
 
 // Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
@@ -20,6 +68,12 @@ if (app.Environment.IsDevelopment())
 }
 
 app.UseHttpsRedirection();
+
+app.UseRouting();
+
+app.UseResponseCaching();
+
+app.UseAuthentication();
 
 app.UseAuthorization();
 
